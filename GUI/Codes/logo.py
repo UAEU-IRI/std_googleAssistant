2f1fca7b93d3a0b5a407396d9ef187920efe92e8
ReemAlsaffar <201601484@uaeu.ac.ae>
2019-03-14 09:50:25 +0400

    GUI

--- GUI/Codes/logo.py
+++ GUI/Codes/logo.py
@@ -1,6 +1,6 @@
 from tkinter import*
 root = Tk()
-photo = PhotoImage(file="L.png")
+photo = PhotoImage(file="/home/pi/std_googleAssistant/GUI/Icons/Home.png")
 label = Label(root, image=photo)
 label.pack()
 root.mainloop()
