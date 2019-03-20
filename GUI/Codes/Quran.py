6a01975f42ec24d2801ad2d1d91f2f841f68cc2e
amnaAlnaimi <45402685+amnaAlnaimi@users.noreply.github.com>
2019-03-20 23:21:01 +0400

    Add files via upload

--- GUI/Codes/Quran.py
+++ GUI/Codes/Quran.py
@@ -7,48 +7,43 @@ def openMRA():
 def openAA():
  webbrowser.open(AA, new=new)
 
-def openAhmedA():
- webbrowser.open(AhmedA, new=new)
+def openAAlN():
+ webbrowser.open(AAlN, new=new)
 
-def openMKA():
- webbrowser.open(MKA, new=new)
+def openHAB():
+ webbrowser.open(HAB, new=new)
 
 class GUI:
     def __init__(self, master):
      frame = Frame(master)
      frame.pack()
      frame.configure(background='white')
-     self.label_1 = Label(frame, text="Mishary Rashid Alafasy", bg="white", font=("Helvetica", 14))
-     self.label_2 = Label(frame, text="Abdul Basset Abdussamad", bg="white", font=("Helvetica", 14))
-     self.label_3 = Label(frame, text="Ahmad bin Ali Al-Ajmi", bg="white", font=("Helvetica", 14))
-     self.label_4 = Label(frame, text="Mahmoud Khalil Al-Hussary", bg="white", font=("Helvetica", 14))
      self.button1 = Button(frame, bg="white",command=openMRA)
      self.button1.config(image=photo1)
      self.button2 = Button(frame, bg="white",command=openAA)
-     self.button2.config(image=photo1)
-     self.button3 = Button(frame, bg="white",command=openAhmedA)
-     self.button3.config(image=photo1)
-     self.button4 = Button(frame, bg="white",command=openMKA)
-     self.button4.config(image=photo1)
-     self.label_1.grid(row=0, column=0)
-     self.label_2.grid(row=1, column=0)
-     self.label_3.grid(row=2, column=0)
-     self.label_4.grid(row=3, column=0)
+     self.button2.config(image=photo2)
+     self.button3 = Button(frame, bg="white",command=openAAlN)
+     self.button3.config(image=photo3)
+     self.button4 = Button(frame, bg="white",command=openHAB)
+     self.button4.config(image=photo4)
      self.button1.grid(row=0, column=1)
-     self.button2.grid(row=1, column=1)
-     self.button3.grid(row=2, column=1)
-     self.button4.grid(row=3, column=1)
+     self.button2.grid(row=0, column=2)
+     self.button3.grid(row=1, column=1)
+     self.button4.grid(row=1, column=2)
     
 root= Tk()
 new = 1
 MRA = "https://soundcloud.com/hussein-abdelkawy/sets/shmnbresddeo"
-AA="https://soundcloud.com/telawatcloud/sets/abdulbasithafs"
-AhmedA = "https://soundcloud.com/telawatcloud/sets/alajmi"
-MKA="https://soundcloud.com/telawatcloud/sets/hosaryqalon"
-photo = PhotoImage(file="/home/pi/std_googleAssistant/GUI/Icons/Quran1.png")
-photo1 = PhotoImage(file="/home/pi/std_googleAssistant/GUI/Icons/Play.png")
-photoB = PhotoImage(file="/home/pi/std_googleAssistant/GUI/Icons/Back.png")
-photoE = PhotoImage(file="/home/pi/std_googleAssistant/GUI/Icons/Exit.png")
+AA="https://soundcloud.com/abdelrhman-farouk-269278979/sets/9o2rlttvrqxq"
+AAlN = "https://soundcloud.com/shahd-abd-el-aziz/sets/q0cyrjk0xsiw"
+HAB="https://soundcloud.com/ahmed-bob-731271110/sets/68gixqmstfdu"
+photo = PhotoImage(file="C:/Users/amoon/Documents/GitHub/std_googleAssistant/GUI/Icons/Quran1.png")
+photo1 = PhotoImage(file="C:/Users/amoon/Documents/GitHub/std_googleAssistant/GUI/Icons/SheikhMisharyAlafasy .png")
+photo2 = PhotoImage(file="C:/Users/amoon/Documents/GitHub/std_googleAssistant/GUI/Icons/SheikhAl-Minsha.png")
+photo3 = PhotoImage(file="C:/Users/amoon/Documents/GitHub/std_googleAssistant/GUI/Icons/AhmadAlnufais.png")
+photo4 = PhotoImage(file="C:/Users/amoon/Documents/GitHub/std_googleAssistant/GUI/Icons/hazzaalbalushi.png")
+photoB = PhotoImage(file="C:/Users/amoon/Documents/GitHub/std_googleAssistant/GUI/Icons/Back.png")
+photoE = PhotoImage(file="C:/Users/amoon/Documents/GitHub/std_googleAssistant/GUI/Icons/Exit.png")
 back = Button(root, bg="white")
 back.config(image=photoB)
 quitButton = Button(root, command=root.quit, bg="white")
