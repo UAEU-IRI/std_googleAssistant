cb1a12031a623f89bbbd1c84ef8268851a611c7c
Reem Alsaffar <45401887+ReemAlsaffar@users.noreply.github.com>
2019-04-17 10:47:57 +0400

    test gif

--- GUI/Codes/TestGirl.py
+++ GUI/Codes/TestGirl.py
@@ -1,9 +1,44 @@
 from tkinter import *
+from PIL import Image, ImageTk
+from itertools import count, cycle
 
-root= Tk()
+class ImageLabel(Label):
+    def load(self, im):
+        if isinstance(im, str):
+            im = Image.open(im)
+        frames = []
+        try:
+            for i in count(1):
+                frames.append(ImageTk.PhotoImage(im.copy()))
+                im.seek(i)
+        except EOFError:
+            pass
+        self.frames = cycle(frames)
+
+        try:
+            self.delay = im.info['duration']
+        except:
+            self.delay = 100
+
+        if len(frames) == 1:
+            self.config(image=next(self.frames))
+        else:
+            self.next_frame()
+
+    def unload(self):
+        self.config(image=None)
+        self.frames = None
+
+    def next_frame(self):
+        if self.frames:
+            self.config(image=next(self.frames))
+            self.after(self.delay, self.next_frame)
+
+root = Tk()
 photo1 = PhotoImage(file="/home/pi/std_googleAssistant/GUI/Icons/GirlMusic.gif")
-label = Label(root, image=photo1)
-label.pack()
+lbl = ImageLabel(root)
+lbl.pack()
+lbl.load(photo1)
 root.title("Intelligent Fellow")
 root.geometry("1000x920")
 root.configure(background='white')
